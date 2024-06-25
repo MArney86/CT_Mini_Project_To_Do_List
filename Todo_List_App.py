6e56e7425674f474b0e7d46edd5bb24e34f66f33
MArney86 <matthew.h.arney.1986@gmail.com>
2024-06-24 23:11:42 -0500

    Initial code upload of basic app framework

---
+++ Todo_List_App.py
@@ -0,0 +1,31 @@
+def add_task(todo_list):
+    pass
+
+def view_tasks(todo_list):
+    pass
+
+def mark_complete(todo_list, task):
+    pass
+
+def delete_task(todo_list, task)
+    pass
+
+todo_list = []
+print("Welcome to the To-Do List App!")
+
+while True:
+    print("Menu:\n1. Add a task\n2. View tasks\n3. Mark a task as complete\n4. Delete a task\n5. Quit")
+    try:
+        user_selection = int(input("Please input the number for the feature you'd like to use: "))
+        if user_selection < 1 or user_selection > 5:
+            pass
+        pass
+    except ValueError:
+        pass
+    else:
+        pass
+    finally:
+        pass
+    
+
+      
